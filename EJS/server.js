bbadadfa5375a548dbfb4e5c1bddfceac3c5ed95
i-shashanksharma <shashanksharma240602005@gmail.com>
2026-02-05 10:13:25 +0530

    [Shashank] add today's task

--- EJS/server.js
+++ EJS/server.js
@@ -8,19 +8,58 @@ app.get("/",(req,res)=> {
     res.render("index");
 })
 
-app.get("/user",(req,res)=> {
-    let userData = {
-        name: "Amrit",
-        age: "23"
+let userData = [
+    {id: 1, name: "Amrit",age: "24"},
+    {id: 2, name: "Rahul",age: "35"},
+    {id: 3, name: "Rakesh",age: "30"},
+];
+
+// app.get("/user",(req,res)=> {
+//     let userData = {
+//         name: "Amrit",
+//         age: "23"
+//     }
+//     res.render("user", {userData})
+// })
+
+app.get("/user", (req,res)=> {
+    res.render("user", {userData});
+});
+
+app.use(express.urlencoded({extended:true}))    // Middlewar
+
+app.post("/api/user", (req,res)=> {
+
+    const {name,age} = req.body;
+
+    let newuserData = {
+        id: userData.length+1,
+        name,
+        age
     }
-    res.render("user", {userData})
+    userData.push(newuserData);
+    res.redirect('/user');
+})
+
+app.get("/api/user/:id",(req,res)=> {
+    const userid = req.params.id;
+
+    const useridx = userData.findIndex((ele)=> ele.id==userid);
+    
+    if(useridx == -1) {
+        return res.send("User Not Found");
+    }
+
+    userData.splice(useridx,1);
+
+    res.redirect("/user");
 })
 
 app.get("/list",(req,res)=> {
 
-    // let arr = ["apple","mango","orange"];
+    let arr = ["apple","mango","orange"];
 
-    let arr = [];
+    // let arr = [];
  
     res.render("list",{arr})
 })
@@ -28,28 +67,3 @@ app.get("/list",(req,res)=> {
 app.listen(3000,()=> {
     console.log("server is running");
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// static server
-// csr
-// ssr
-// template engine
-// ejs, pug, hbs
-// seo friendly
