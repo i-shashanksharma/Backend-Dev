8e2b7e5987d9c49695a0705d2874061462dec115
i-shashanksharma <shashanksharma240602005@gmail.com>
2026-03-26 09:26:19 +0530

    [Shashank] add Today's Task

---
+++ Mongo/controllers/authController.js
@@ -0,0 +1,28 @@
+import user from "../model/UserSchema.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hasedpassword = await bcrypt.hash(password,salt);
+
+    const newUser = await user.create({
+      name,
+      email,
+      password:hasedpassword,
+    });
+
+    res.status(200).json({
+      message: "user is created",
+      newUser,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "internal server error",
+      error:error.message
+    });
+  }
+};
